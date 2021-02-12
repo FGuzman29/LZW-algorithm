529e3682e8a31a0131050a51c8fa78de1d12d1cb
Andrés Estrella <57234183+andresestrellaa@users.noreply.github.com>
2021-02-12 03:37:32 -0400

    command line arguments
    agregue codigo para tomar argumentos en la linea de comando
    
    #todo que en modo -c itere sobre los otros argumentos que se le pase al comando y hacer un loop para que se ejecute la funcion de comprimir sobre los archivos especificados en los argumentos

--- LZW-Compresser.py
+++ LZW-Compresser.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+import getopt, sys, re
+from os import path
+
 counter = 256
 charTable = {}
 
@@ -18,8 +22,44 @@ def compression(inputFile):
             p = c
     return result
 
-def main():
-    print(compression("WYS*WYGWYS*WYSWYSG"))
+def decompress(object):
+    print("dosoemthing")
 
-main()
+def main():
+        # quita el primer argumento (que es *.py)
+    arguments = sys.argv[1:]
+    
+    # definicion de los flags
+    options = "c:d:h:"
+
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(arguments, options)
+        
+        # checking each argument
+        for current_argument, current_value in arguments:
+            
+            if current_argument in ("-c"): 
+                #print(compression("WYS*WYGWYS*WYSWYSG"))        
+                with open(current_value) as f:
+                    compression(f.read())
+
+            elif current_argument in ("-d"):   
+                decompress(current_value)
+            
+            elif current_argument in ("-h"): 
+                print('COMMANDS: \n compress mode: -c followed by as many files and directories as you want \n decompress mode: -d followed by a single .lzw file')
+                
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+
+    if arguments[0] not in("-c","-d","-h"):
+                print(arguments[0])
+            
+            
+            
+            
+if __name__ == "__main__":
+    main()
             
